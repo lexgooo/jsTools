96dd1d53145c3bdfab59fe47b51921a476c05f55
顾超 <guchao@guchaodeiMac.local>
2019-05-24 09:13:49 +0800

    修改文件名,改为node的引入方式

--- checkId.js
+++ checkId.js
@@ -3,7 +3,6 @@ function getCheckCode(id) {
     const checkCodes = [1, 0, 'x', 9, 8, 7, 6, 5, 4, 3, 2]
     id = id.split('')
     id.pop()
-    // console.log('id array', id)
     let total = 0
     id.forEach((item, i) => {
         total += (+item)*weightingFactors[i]
@@ -11,7 +10,7 @@ function getCheckCode(id) {
     return checkCodes[total%11]
 }
 
-export function checkId(id) {
+function checkId(id) {
     const provinceCodes = [11, 12, 13, 14, 15, 
         21, 22, 23,
         31, 32, 33, 34, 35, 36, 37,
@@ -20,10 +19,9 @@ export function checkId(id) {
         61, 62, 63, 64, 65, 
         81, 82, 83]
     let provinces = provinceCodes.join('|')
-    console.log('provinces', provinces)
     const checkCode = getCheckCode(id)
-    console.log('checkCode', checkCode)
     const reg = new RegExp(String.raw`^(${provinces})\d{4}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])(\d{3})${checkCode}$`, 'i')
-    console.log('reg', reg)
     return reg.test(id)
 }
+
+exports.checkId = checkId
